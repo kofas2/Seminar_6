7edad2e1b0544f761a8505507b21dad4dac8c2e4
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-29 18:07:59 +0300

    add Task005 Seminar 6

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -4,51 +4,6 @@
 // Если N = 7 -> 0 1 1 2 3 5 8
 
 
-// int MessageToUser(string message)
-// {
-//     System.Console.Write(message);
-//     string userInput = System.Console.ReadLine();
-//     int result = int.Parse(userInput);
-//     return result;
-// }
-
-
-// void Fibonacci(int [] array)
-// {   
-//     array[0] = 0;
-//     array[1] = 1;
-//     for (int i = 2; i < array.Length; i++)
-//     {
-//         array[i] = array[i-1]+array[i-2];
-//     }
-// }
-
-
-// void PrintArray(int[]array)
-// {
-//     foreach (int item in array)
-//     {
-//         Console.Write($"{item} ");
-//     }
-//     Console.WriteLine();
-// }
-
-
-
-
-// int num = MessageToUser("Введите целое число: ");
-// int []array = new int[num];
-// Fibonacci(array);
-// PrintArray(array);
-
-
-// Задача НЕГАФИБОНАЧЧИ. Задайте число. 
-// Составьте массив чисел НегаФибоначчи, 
-// в том числе для отрицательных индексов.
-// *Пример:*
-// - для k = 9 массив будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
-
-
 int MessageToUser(string message)
 {
     System.Console.Write(message);
@@ -58,8 +13,8 @@ int MessageToUser(string message)
 }
 
 
-void Fibonacci(int[] array)
-{
+void Fibonacci(int [] array)
+{   
     array[0] = 0;
     array[1] = 1;
     for (int i = 2; i < array.Length; i++)
@@ -68,39 +23,22 @@ void Fibonacci(int[] array)
     }
 }
 
-void Fibonacci2(int[] array, int [] array2)
+
+void PrintArray(int[]array)
 {
-    for (int i = 0; i<array2.Length; i++)
+    foreach (int item in array)
     {
-        array2[i] -= array[array.Length-1-i];
+        Console.Write($"{item} ");
     }
-    for (int i = 1; i<array2.Length;i+=2)
-    {
-        array2[i]*=-1;
-    }
+    Console.WriteLine();
 }
 
 
-    void PrintArray(int[] array, int []array2)
-    {
-        foreach (int item in array2)
-        {
-            Console.Write($"{item} ");
-        }
-        Console.Write("");
-        foreach (int item in array)
-        {   
-            if (item!=0)
-                Console.Write($"{item} ");
-        }
-    }
 
 
+int num = MessageToUser("Введите целое число: ");
+int []array = new int[num];
+Fibonacci(array);
+PrintArray(array);
 
 
-    int num = MessageToUser("Введите целое число: ");
-    int[] array = new int[num];
-    int[] array2 = new int[num];
-    Fibonacci(array);
-    Fibonacci2(array, array2);
-    PrintArray(array, array2);
---
+++ Task005/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача НЕГАФИБОНАЧЧИ. Задайте число. 
+// Составьте массив чисел НегаФибоначчи, 
+// в том числе для отрицательных индексов.
+// *Пример:*
+// - для k = 9 массив будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+
+int MessageToUser(string message)
+{
+    System.Console.Write(message);
+    string userInput = System.Console.ReadLine();
+    int result = int.Parse(userInput);
+    return result;
+}
+
+
+void Fibonacci(int[] array)
+{
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i-1]+array[i-2];
+    }
+}
+
+void Fibonacci2(int[] array, int [] array2)
+{
+    for (int i = 0; i<array2.Length; i++)
+    {
+        array2[i] -= array[array.Length-1-i];
+    }
+    for (int i = 1; i<array2.Length;i+=2)
+    {
+        array2[i]*=-1;
+    }
+}
+
+
+    void PrintArray(int[] array, int []array2)
+    {
+        foreach (int item in array2)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.Write("");
+        foreach (int item in array)
+        {   
+            if (item!=0)
+                Console.Write($"{item} ");
+        }
+    }
+
+
+
+
+    int num = MessageToUser("Введите целое число: ");
+    int[] array = new int[num];
+    int[] array2 = new int[num];
+    Fibonacci(array);
+    Fibonacci2(array, array2);
+    PrintArray(array, array2);
