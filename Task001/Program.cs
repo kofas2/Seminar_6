663c76f15291717ea8f4746c945d5a0a5dcb5f1d
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-29 06:40:54 +0300

    add Task002 Seminar 6

---
+++ Task001/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+void FillArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10,100);
+    }
+}
+
+void PrintArray(int []array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+void ReverseArray(int [] array)
+{
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        (array[i] , array [array.Length - 1 -i]) = (array[array.Length - 1 -i] , array[i]); // множественное присваивание
+    }
+    // {
+    //     int temp = array [i];
+    //     array[i] = array [array.Length - 1 -i];
+    //     array [array.Length -1 -i] = temp; 
+    // }
+}
+
+
+
+
+Console.WriteLine("Введите размерность массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [size];
+FillArray(array);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
+
---
+++ Task002/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и п 
+// роверяет, может ли существовать треугольник с сторонами такой длины. 
+
+
+int MessageToUser(string message)
+{
+    System.Console.Write(message);
+    string userInput = System.Console.ReadLine();
+    int result = int.Parse(userInput);
+    return result;
+}
+
+void ChekTriangle(int a, int b, int c)
+{
+    if (a<b+c && b<a+c && c<a+b)
+    {
+        Console.WriteLine($"Треугольник со сторонами {a}, {b}, {c} - существует");
+    }
+    else 
+    {
+        Console.WriteLine("Такого треугольника не существует");
+    }
+}
+
+// bool triangleExists (int a, int b, int c) 
+// { 
+//     if ((a + b > c) && (a + c > b) && (b + c > a)) return true; 
+//     else return false; 
+// }
+
+
+
+
+//AB<AC+CB - теорема о неравенстве треугольника
+int a = MessageToUser("Введите длину первой стороны: ");
+int b = MessageToUser("Введите длину первой стороны: ");
+int c = MessageToUser("Введите длину первой стороны: ");
+ChekTriangle(a,b,c);
